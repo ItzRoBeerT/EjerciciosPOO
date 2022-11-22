6b203ee62a2d5b5d467df88193059401a7201e74
Roberto <105317145+ItzRoBeerT@users.noreply.github.com>
2022-11-22 13:33:55 +0100

    a

--- Ejercicio5/Test.py
+++ Ejercicio5/Test.py
@@ -1,5 +1,4 @@
 from Ejercicio5.CajaAhorro import CajaAhorro
-from Ejercicio5.Cuenta import Cuenta
 from Ejercicio5.PlazoFijo import PlazoFijo
 
 cuenta1 = PlazoFijo("Roberto", 1200,4,300)
@@ -8,3 +7,4 @@ print(cuenta1)
 
 cuenta2 = CajaAhorro("Juan",1500)
 print(cuenta2)
+
