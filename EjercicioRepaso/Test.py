bc01d4e8b5c4d71442e4e636c5fe00d3246b295b
Roberto <105317145+ItzRoBeerT@users.noreply.github.com>
2022-11-23 14:13:03 +0100

    terminado

--- EjercicioRepaso/Test.py
+++ EjercicioRepaso/Test.py
@@ -1,23 +1,50 @@
 from random import randint
 def generarMenu():
     print('''
-           Color                    DESCUENTO
-        
-        Bola Blanca                No tiene
-        Bola Roja                  10 por ciento
-        Bola Azul                  20 por ciento
-        Bola Verde                 25 por ciento
-        Bola Amarilla              50 por ciento
+        * * * * * * * * * * * * * * * * * * * * *
+       *    Color                    DESCUENTO    *
+       *                                          *
+       * Bola Blanca                No tiene      * 
+       * Bola Roja                  10 por ciento *
+       * Bola Azul                  20 por ciento * 
+       * Bola Verde                 25 por ciento *
+       * Bola Amarilla              50 por ciento *
+        * * * * * * * * * * * * * * * * * * * * * 
     ''')
 
 dinero = input("Introduzca cantidad a pagar ")
-
+descuento= 0
 if float(dinero) <100:
     print("No se aplica promoción")
 else:
+    generarMenu()
     numAl = randint(0,5)
+    if numAl ==0:
+        print("Te ha tocado la blanca")
+    elif numAl == 1:
+        print("Te ha tocado la roja")
+        descuento= 10
+    elif numAl ==2:
+        print("Te ha tocado la azul")
+        descuento = 20
+    elif numAl ==3:
+        print("Te ha tocado la verde")
+        descuento = 25
+    elif numAl ==4:
+        print("Te h tocado la amarilla")
+        descuento = 50
 
-generarMenu()
+if  descuento == 0:
+    print(f"No ha conseguido ningun descuento\nPrecio: {dinero}$")
+else:
+    dineroDesc = (float(dinero)*descuento)/100
+    dinero = float(dinero)-dineroDesc
+    print(f"Ha coneguido un descuento de {descuento}%\nNuevo precio: {dinero}$")
+
+
+
+
+
 
 
 
